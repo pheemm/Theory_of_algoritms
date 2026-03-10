9010255ec1468293129766895850853b1c016118
pheemm <98599136+pheemm@users.noreply.github.com>
2026-03-10 23:47:21 +0300

    Add files via upload

---
+++ competitive_placement.py
@@ -0,0 +1,47 @@
+def is_safe(v, selected_nodes, graph):
+    if v in selected_nodes:
+        return False
+    
+    for neighbor in graph[v]:
+        if neighbor in selected_nodes:
+            return False
+        
+    return True
+
+def can_p2_win(selected, p2_score, turn_p1):
+    available = []
+    for v in graph:
+        if is_safe(v, selected, graph):
+            available.append(v)
+
+    if not available:
+        return p2_score >= target_B
+    
+    if turn_p1:
+        for move in available:
+            if not can_p2_win(selected + [move], p2_score, False):
+                return False
+        return True
+    
+    else:
+        for move in available:
+            if can_p2_win(selected + [move], p2_score + weights[move], True):
+                return True
+        return False
+
+graph = {0 :[1],
+         1 :[0, 2],
+         2 :[1, 3],
+         3 :[2, 4],
+         4 :[3, 5],
+         5 :[4, 6],
+         6 :[5, 7],
+         7 :[6, 8],
+         8 :[7, 9],
+         9 :[8]}
+
+weights = [10, 1, 5, 15, 5, 1, 5, 1, 15, 10]
+
+target_B = 15
+
+print(can_p2_win([], 0, True))
